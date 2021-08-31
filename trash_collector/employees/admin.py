714f923684ef7e4374aa22a5d7e17197fe7c2ddb
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-08-31 17:40:14 -0400

    Remove duplicates

--- trash_collector/employees/admin.py
+++ trash_collector/employees/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 # Register your models here.
 
-# Filter method as a class or seperate file?
 
 class Filter(admin.SimpleListFilter):
     list_display = ("Monday", "Tuesday", "Wednesday", "Thursday", )
--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,7 +1,12 @@
 # from trash_collector.employees.models import Employees
+from trash_collector.customers.models import Customer
+from trash_collector import employees
+from trash_collector.customers.views import User
+from trash_collector.employees.models import Employees
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.apps import apps
+from django.apps import app
+from .filters import TodaysCustomersFilter
 
 # Create your views here.
 
@@ -13,9 +18,34 @@ def index(request):
     Customer = apps.get_model('customers.Customer')
     return render(request, 'employees/index.html')
 
-def filter(request):
-    pass
+def filter(request, user_id):
+    user = User.objects.get(pk=user_id)
+    employees = Employees.objects.get(user=user)
+    return render(request, 'employees/filter.html')
 
+customer_filter= TodaysCustomersFilter() 
 
-def update(request):
-  pass
+context = {'customer', 'zip_code', 'weekly_pickup_day', 'one_time_pickup', 'suspend_start', 'suspend_end',
+        'customer_filter'
+    }
+    return render(request, 'update.html', context)
+
+# def update(request, user_id):
+#     user = User.objects.get(pk=user_id)
+#     customer = Employees.objects.get(user=user)
+#      if request.method == 'POST':
+#         employees.name = request.POST.get('name')
+#         employees.zip_code = request.POST.get('zipcode')
+#         return render(request, 'customers/update.html')
+        
+def update(request, user):
+    user= request.user
+    logged_in_employee=Employees.objects.get(user=user)
+    
+    if request.method == 'POST':
+        logged_in_employee.name = request.POST.get('name')
+        logged_in_employee.zip_code = request.POST.get('zipcode')
+        logged_in_employee.user = request.POST.get('user')
+        return render(request, 'employees/update.html')
+
+  
