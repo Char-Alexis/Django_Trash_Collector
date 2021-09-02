a05e940d21bcd05b61c8a56cd03f3ce5eabd8eb4
charmaine llacuna <charmaine.llacuna@yahoo.com>
2021-09-01 21:30:35 -0400

    Updated days of the week under choices.py and included bootstrap components

--- trash_collector/customers/choices.py
+++ trash_collector/customers/choices.py
@@ -3,3 +3,8 @@ from django.db import models
 class DayOfTheWeek(models.TextChoices):
     MONDAY = 'Monday'
     TUESDAY = 'Tuesday'
+    WEDNESDAY = 'Wednesday'
+    THURSDAY = 'Thursday'
+    FRIDAY = 'Friday'
+    SATURDAY = 'Saturday'
+    SUNDAY = 'Sunday'
